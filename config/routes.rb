f26edb87b3230ea2c7746a39dfc39b200bbb3af7
Jean Daniel DULOIR <jdduloir@icloud.com>
2017-08-07 17:06:37 +0200

    oui oui oui

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/game' => 'longer_word#game'
+
+  get '/score' => 'longer_word#score'
+
+  root to: 'longer_word#game'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
